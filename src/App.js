e1c859a19379b564346873e3bbb98d3f7119c938
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-12 10:13:03 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { PortfolioContext } from './context/context';
 
 function App() {
+  const [portfolio, setPortfolio] = useState([]);
+
+  /*useEffect(() => {
+    async function getData() {
+      const resp = await axios.get("../creaciones.json");
+      console.log("LMAO", resp);
+      setContext(resp);
+    }
+    
+    getData();
+  }, []);*/
+
+  const añadirAFavs = (item) => {
+    //añadir a localstorage
+    setPortfolio(portfolio.map(obra => obra.nombre === item.nombre ? {...obra, favorito: true} : obra));
+  }
+
+  const eliminarDeFavs = (item) => {
+    //eliminar de localStorage
+    setPortfolio(portfolio.map(obra => obra.nombre === item.nombre ? {...obra, favorito: false} : obra));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <Topbar></Topbar>
+      <PortfolioContext.Provider value={portfolio}>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/sobremi" element={<AboutMe />} />
+          <Route path="/creaciones" element={<Creaciones />} />
+          <Route path="/favs" element={<Favs />} />
+        </Routes>
+      </PortfolioContext.Provider>
+      <Footer></Footer>
+
+    </BrowserRouter>
   );
 }
 
